f8816a01ed0e496d21de3451243f8993a6fcf4de
abbashir <bashir.vu@gmail.com>
2019-02-23 19:20:13 +0600

    created model -a

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,12 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::apiResource('/Products','ProductController');
+
+Route::group(['prefix' => 'Products'], function(){
+
+    Route::apiResource('/{Product}/reviews','ReviewController');
+
+    });
