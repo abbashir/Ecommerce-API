a8950a687d6054393ce0f981e2dc8af4ab7d4854
abbashir <bashir.vu@gmail.com>
2019-02-25 20:52:01 +0600

    midified product details

--- app/Http/Resources/Product/productResource.php
+++ app/Http/Resources/Product/productResource.php
@@ -18,9 +18,17 @@ class productResource extends JsonResource
             'name' => $this->name,
             'description' => $this->detail,
             'price' => $this->price,
-            'stock' => $this->stock,
-            'discount' => $this->discount
+            'stock' => $this->stock == 0 ? 'out of stock':$this->stock,
+            'discount' => $this->discount,
+            'Total_price' => round(($this->price/100)*(100 - $this->discount),2),
+
+            'rating' => $this->reviews->count() >0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'No rating yet',
+
+            'href' => [
+                'reviews' => route('reviews.index',$this->id)
+            ]
 
         ];
     }
 }
+
